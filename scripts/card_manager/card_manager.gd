d13a89736d4ce861f63a270bd764f1088249694b
Bruno-Calasans <brunogstsho@gmail.com>
2024-11-27 23:35:49 -0300

    create card slot

--- scripts/card_manager/card_manager.gd
+++ scripts/card_manager/card_manager.gd
@@ -6,12 +6,16 @@ class_name CardManager
 var hovering_card = false # can be one hover effect at once
 var dragged_card: Node2D = null
 
+@export_category('Collsion Masks')
+const COLLISION_MASK_CARD = 1
+const COLLISION_MASK_CARD_SLOT = 2
+
 func _input(event: InputEvent) -> void:
 	
 	if event.is_action_pressed('drag'):
 		var card = check_for_card()
 		
-		if card and card.name.contains('Card'):
+		if card is Card:
 			start_drag(card)
 		
 	if event.is_action_released('drag'):
@@ -22,7 +26,7 @@ func check_for_card():
 	var space = get_world_2d().direct_space_state
 	var point_parameters = PhysicsPointQueryParameters2D.new()
 	point_parameters.collide_with_areas = true
-	point_parameters.collision_mask = 1
+	point_parameters.collision_mask = COLLISION_MASK_CARD
 	point_parameters.position = get_global_mouse_position()
 	
 	var results = space.intersect_point(point_parameters)
@@ -36,6 +40,21 @@ func check_for_card():
 		
 	return get_card_on_top(cards)
 		
+func check_for_card_slot() -> CardSlot:
+	var space = get_world_2d().direct_space_state
+	var point_parameters = PhysicsPointQueryParameters2D.new()
+	point_parameters.collide_with_areas = true
+	point_parameters.collision_mask = COLLISION_MASK_CARD_SLOT
+	point_parameters.position = get_global_mouse_position()
+
+	var results = space.intersect_point(point_parameters)
+	if results.size() == 0: return null
+	
+	var card_slot = results[0].collider.get_parent() 
+	if card_slot is not CardSlot: return null
+	
+	return card_slot
+	
 # drags the card and limit its position to the screen's size
 func drag_card():
 	if dragged_card:
@@ -43,7 +62,6 @@ func drag_card():
 		var card_size = card_area.shape.get_rect().size
 		var card_width = card_size.x
 		var card_height = card_size.y
-		print(card_size)
 		
 		var mouse_pos = get_global_mouse_position()
 		var x_pos = clamp(mouse_pos.x, card_width / 2, screen_size.x - card_width / 2)
@@ -52,12 +70,29 @@ func drag_card():
 
 # keep the drag effect while the card is being hovered over
 func start_drag(card: Card):
+	# card being moved out a card slot
+	"var card_slot_found = check_for_card_slot()
+	print(card_slot_found)
+	print(card_slot_found.occupied_card.get_instance_id())
+	var is_exiting_card_slot = card_slot_found.occupied_card.get_instance_id() == card.get_instance_id()
+	if card_slot_found and is_exiting_card_slot:
+		card_slot_found.ocuppied = false
+		card_slot_found.occupied_card = null"
+		
 	dragged_card = card
 	dragged_card.scale = Vector2(1.05, 1.05)
 	
 # stops the drag effect while the hovering stops
 func stop_drag():
 	if dragged_card:
+		# card dropped in a card slot
+		var card_slot_found = check_for_card_slot()
+		if card_slot_found and not card_slot_found.ocuppied:
+			var card_slot_area = card_slot_found.get_node('./CardSlotCollisionArea')
+			dragged_card.global_position = card_slot_area.global_position
+			card_slot_found.ocuppied = true
+			card_slot_found.occupied_card = dragged_card
+			
 		dragged_card.scale = Vector2(1, 1)
 		dragged_card = null
 
@@ -90,7 +125,7 @@ func highlight_card(card: Card, hovered: bool):
 		card.z_index = 1
 
 func get_card_on_top(cards: Array[Card]):
-	
+	if cards.size() == 0: return
 	var top_card = cards[0]
 	var top_z_index = cards[0].z_index
 	
---
+++ scripts/card_slot/card_slot.gd
@@ -0,0 +1,5 @@
+extends Node
+class_name CardSlot
+
+var ocuppied = false
+var occupied_card: Card
